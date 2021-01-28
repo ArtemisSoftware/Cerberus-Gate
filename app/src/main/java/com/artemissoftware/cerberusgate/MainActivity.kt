c92375fab6cfa5487f149d813d52d66b348f8ae6
gustavo <gustavo.isaac@gmail.com>
2021-01-28 12:50:34 +0000

    OtpFragment.kt

--- app/src/main/java/com/artemissoftware/cerberusgate/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/cerberusgate/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.artemissoftware.cerberusgate
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var bottomNavigation: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(this)
+
+        openFragment(OtpFragment());
+    }
+
+
+    fun openFragment(fragment: Fragment) {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.navigation_auth_one -> {
+                openFragment(OtpFragment())
+                return true
+            }
+            R.id.navigation_auth_two -> {
+                //openFragment(SmsFragment.newInstance("", ""))
+                return true
+            }
+
+        }
+        return false
     }
 }
